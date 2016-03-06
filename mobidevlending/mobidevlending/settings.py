35452e7066502618450fe9475cc56004a1175580
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-03-06 06:49:14 +0300

    added rest_framework in the installed apps, and authentication setting

--- mobidevlending/mobidevlending/settings.py
+++ mobidevlending/mobidevlending/settings.py
@@ -38,9 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+
     'mobidevlending.modules.blog',
     'mobidevlending.modules.ussd'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGE_SIZE': 10
+}
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
