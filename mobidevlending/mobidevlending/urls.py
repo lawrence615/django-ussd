2e235af30c112e03dfb741887777c0514fa3965d
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-03-06 06:48:41 +0300

    added DRF api-auth route, api app route and code indentation

--- mobidevlending/mobidevlending/urls.py
+++ mobidevlending/mobidevlending/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework import routers
+from modules.api import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UssdUserViewSet)
 
 urlpatterns = [
-    url(r'^ussd/', include('mobidevlending.modules.ussd.urls')),
     url(r'', include('mobidevlending.modules.blog.urls')),
+    url(r'^ussd/', include('mobidevlending.modules.ussd.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
